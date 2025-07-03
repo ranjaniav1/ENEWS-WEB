b0f2f80c9c25f0399d9ddf6e2538a2cc2b54d6d4
Varsani2520 <varsaniranjani@gmail.com>
2025-07-03 10:09:18 +0530

    ads adjustment

--- app/components/shared/Banner.jsx
+++ app/components/shared/Banner.jsx
@@ -1,20 +1,34 @@
 "use client";
 import React from "react";
-
+import { Container } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useThemeContext } from "@/app/context/ThemeContext";
 import GoogleAd from "../features/GoogleAd";
 
 const Banner = ({ title, href }) => {
- 
+  const { config, settings } = useThemeContext();
+  const router = useRouter();
 
   return (
-  
+    <Container
+      maxWidth="xl"
+      disableGutters
+      className="mt-4"
+      sx={{
+        display: { xs: "none", sm: "none", md: "flex" },
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90px",
+        width: "100%",
+      }}
+    >
+      <div className="flex justify-center items-center w-[1200px] h-[90px] rounded-lg overflow-hidden shadow-lg">
         <GoogleAd
-                 adSlot="1338579894"
-                 style={{ display: "inline-block", width: 1200, height: 90 }}
-               />
-     
-  
-
+          adSlot="1338579894"
+          style={{ display: "inline-block", width: 1200, height: 90 }}
+        />
+      </div>
+    </Container>
   );
 };
 
