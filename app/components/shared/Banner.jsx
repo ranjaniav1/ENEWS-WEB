db51be9de1dbfcdf5fefe0a45513a57b5ae208e3
Varsani2520 <varsaniranjani@gmail.com>
2025-07-03 10:18:23 +0530

    removed container

--- app/components/shared/Banner.jsx
+++ app/components/shared/Banner.jsx
@@ -1,20 +1,10 @@
 "use client";
 import React from "react";
-import { Container } from "@mui/material";
 import GoogleAd from "../features/GoogleAd";
 
 const Banner = () => {
   return (
-    <Container
-      maxWidth="xl"
-      disableGutters
-      sx={{
-        display: { xs: "none", sm: "none", md: "flex" },
-        justifyContent: "center",
-        alignItems: "center",
-        py: 2,
-      }}
-    >
+   
       <div className="w-full max-w-[1200px] h-[90px] mx-auto flex justify-center items-center rounded-lg overflow-hidden shadow-lg bg-white">
         <GoogleAd
           adSlot="1338579894"
@@ -25,7 +15,7 @@ const Banner = () => {
           }}
         />
       </div>
-    </Container>
+  
   );
 };
 
