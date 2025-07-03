4735b97ba98fb0660b16f8ff43e7c23aa0b42da2
Varsani2520 <varsaniranjani@gmail.com>
2025-07-03 10:05:46 +0530

    remove container

--- app/components/shared/Banner.jsx
+++ app/components/shared/Banner.jsx
@@ -1,29 +1,19 @@
 "use client";
 import React from "react";
-import { Container, Button, Typography } from "@mui/material";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useThemeContext } from "@/app/context/ThemeContext";
+
 import GoogleAd from "../features/GoogleAd";
 
 const Banner = ({ title, href }) => {
-  const { config, settings } = useThemeContext();
-  const router = useRouter();
+ 
 
   return (
-    <Container
-      maxWidth="xl"
-      disableGutters
-      className="mt-4"
-      sx={{ display: { xs: "none", sm: "none", md: "block" },height:"90px",width:"1200px"}}
-    >
-      <div className="w-full h-full rounded-lg overflow-hidden shadow-lg">
+  
         <GoogleAd
                  adSlot="1338579894"
                  style={{ display: "inline-block", width: 1200, height: 90 }}
                />
-      </div>
-    </Container>
+     
+  
 
   );
 };
